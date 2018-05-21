7804763c2470946547970c63a7055581bd858f3c
carver_x <carver_x@163.com>
2018-05-21 15:00:08 +0800

    修改变量名防止变量名冲突

--- baiduDisk.sh
+++ baiduDisk.sh
@@ -3,17 +3,17 @@
 # 参数：目录的绝对路径；例如：'西==瓜[[豆==芽'
 # 功能：生成'西==瓜[[豆==芽.file'，其中包含该目录的所有子目录
 function genSubdirOfNameToFile() {
-	PARAM1=${1//[[//}
-	PARAM=${PARAM1//==/\\ }	
-	PREFIX=$1
-	FILENAME=${1##*[[}
+	G_PARAM1=${1//[[//}
+	G_PARAM=${G_PARAM1//==/\\ }	
+	G_PREFIX=$1
+	G_FILENAME=${1##*[[}
 # 将路径前缀存入文件的首行
-	echo $PREFIX > $FILENAME.file
-	echo $PARAM | xargs bypy list > tmp
+	echo $G_PREFIX > $G_FILENAME.file
+	echo $G_PARAM | xargs bypy list > tmp
 	sed -i 1d tmp
 	sed -i s/\ $//g tmp
 	sed -i s/\ /==/g tmp
-	cat tmp | awk -F == '{$NF=$(NF-1)=$(NF-2)=""; print $0}' >> $FILENAME.file && rm -f tmp
+	cat tmp | awk -F == '{$NF=$(NF-1)=$(NF-2)=""; print $0}' &>> $G_FILENAME.file && rm -f tmp
 }
 
 # 参数：西__瓜[[豆__芽
@@ -21,29 +21,30 @@ function genSubdirOfNameToFile() {
 function recursion() {
 	genSubdirOfNameToFile $1
 
-	sed 1d $FILENAME.file | while read line; do
-		FLAG=${line:0:1}
-		DIRNAME1=${line:2}
+	R_FILE=${1##*[[}.file
+	sed 1d $R_FILE | while read line; do
+		R_FLAG=${line:0:1}
+		R_DIRNAME1=${line:2}
 		# 特殊字符处理
-		DIRNAME2=${DIRNAME1// /==}
-		DIRNAME3=${DIRNAME2////[[}
-		DIRNAME=${DIRNAME3//\'/\\\'}
+		R_DIRNAME2=${R_DIRNAME1// /==}
+		R_DIRNAME3=${R_DIRNAME2////[[}
+		R_DIRNAME=${R_DIRNAME3//\'/\\\'}
 
-		FILENAME=${1##*[[}
-		PREFIX=`head -1 $FILENAME.file`
+		R_FILENAME=${1##*[[}
+		R_PREFIX=`head -1 $R_FILENAME.file`
 
-		if [ $FLAG == 'D' ]; then
-			WHOLEDIRNAME=$PREFIX[[${DIRNAME//\ /\\ }
-			recursion $WHOLEDIRNAME
+		if [ $R_FLAG == 'D' ]; then
+			R_WHOLEDIRNAME=$R_PREFIX[[${R_DIRNAME//\ /\\ }
+			recursion $R_WHOLEDIRNAME
 		else
-			echo $PREFIX[[$line | awk '{$NF=""; print $0}' >> /opt/WangKe.txt
+			echo $R_PREFIX[[$line | awk '{$NF=""; print $0}' >> /opt/WangKe.txt
 		fi
 	done
 }
 
-ROOTDIR='西==瓜[[豆==芽'
+#ROOTDIR='西==瓜[[豆==芽'
 #ROOTDIR='17年赛普健身学院文件'
-#ROOTDIR='test'
+ROOTDIR='test'
 if [ ! -d WangKe-tmp ]; then
 	mkdir WangKe-tmp
 fi
